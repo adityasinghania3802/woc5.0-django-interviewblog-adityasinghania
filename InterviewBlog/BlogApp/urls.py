45fdfe5aa12e3e01c72a4871018aa0df9876cd2d
adityasinghania3802 <adityasinghania3802@gmail.com>
2023-02-04 23:56:24 +0530

    Added Reset Password Functionality

--- InterviewBlog/BlogApp/urls.py
+++ InterviewBlog/BlogApp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index, name="index"),
@@ -21,5 +22,9 @@ urlpatterns = [
     path('deletepost/<int:pid>', views.deletepost, name="deletepost"),
     path('viewpost/<int:pid>/addcomment', views.addcomment, name="addcomment"),
     path('editprofile/',views.editprofile,name="editprofile"),
+    path('reset_password/',auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
 ]
